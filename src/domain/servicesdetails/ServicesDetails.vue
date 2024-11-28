1967b433c079550513b0d739c86b669584ee3296
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-28 15:33:47 +0300

    makes dashboard tiles

--- src/domain/servicesdetails/ServicesDetails.vue
+++ src/domain/servicesdetails/ServicesDetails.vue
@@ -3,12 +3,11 @@ import { ref, type Ref } from "vue";
 import BackofficeAccounts from "@/domain/accounts/BackofficeAccounts.vue";
 import UserAccounts from "@/domain/accounts/UserAccounts.vue";
 
-const activeTab: Ref<string> = ref("backoffice")
+const activeTab: Ref<string> = ref("backoffice");
 
 function select(tab: string) {
-  activeTab.value = tab
+  activeTab.value = tab;
 }
-
 </script>
 
 <template>
@@ -17,7 +16,41 @@ function select(tab: string) {
       <div class="w-full py-1">
         <!-- <i class="bg-primary-700 border border-primary-800 text-white p-2 rounded-full fa-solid fa-list"></i> -->
         <label class="text-xl mx-1 font-semibold">Services We Provide</label>
-        <p class="ml-1">Detailing all the services we offer with small notes on each but also the price for all</p>
+        <p class="ml-1">
+          Detailing all the services we offer with small notes on each but also
+          the price for all
+        </p>
+
+        <div class="row">
+          <div class="mx-auto my-3 col-12">
+            <table class="w-full leading-normal text-sm">
+              <thead>
+                <tr class="border-b border-t border-gray-400 h-10">
+                  <th class="px-4 py-2 text-left t-header">Name</th>
+                  <th class="px-4 py-2 text-left t-header">Type</th>
+                  <th class="px-4 py-2 text-right t-header">Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(item, index) in tableData"
+                  :key="index"
+                  :class="
+                    index % 2 === 0
+                      ? 'bg-gray-100'
+                      : 'hover:bg-gray-200' + ' border-t border-gray-400'
+                  "
+                >
+                  <td class="px-4 py-2 text-gray-600">{{ item.name }}</td>
+                  <td class="px-4 py-2 text-gray-600 capitalize">
+                    {{ item.type }}
+                  </td>
+                  <td class="px-4 py-2 text-right mr-2">{{ item.amount }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -37,12 +70,11 @@ function select(tab: string) {
   @apply w-full px-1 my-2;
 }
 
-.tab{
+.tab {
   @apply cursor-pointer p-2 border-b border-gray-300 text-sm text-center;
 }
 
-.tab-active{
+.tab-active {
   @apply cursor-pointer p-2 border-b-4 border-primary-700 text-sm text-center;
 }
-
 </style>

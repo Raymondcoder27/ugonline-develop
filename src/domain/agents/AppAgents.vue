65c15975a05dcb43392a7883c45c5aad2a47d2c0
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-28 16:30:17 +0300

    adds services details

--- src/domain/agents/AppAgents.vue
+++ src/domain/agents/AppAgents.vue
@@ -15,7 +15,7 @@ function select(tab: string) {
   <div class="flex flex-col w-full shadow-lg bg-white rounded p-2 h-full">
     <div class="flex">
       <div class="w-full py-1">
-        <i class="bg-primary-700 border border-primary-800 text-white p-2 rounded-full fa-solid fa-users"></i>
+        <i class="bg-primary-700 border border-primary-800 text-white p-2 rounded-full fa-solid fa-people-group"></i>
         <label class="text-lg mx-1">System Agents</label>
       </div>
     </div>

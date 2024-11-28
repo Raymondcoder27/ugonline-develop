5055f4c49b321c1f065ae71fbc28e28c24bf2181
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-28 16:56:00 +0300

    adds services details

--- src/domain/agents/BackofficeAccounts.vue
+++ src/domain/agents/BackofficeAccounts.vue
@@ -142,7 +142,8 @@ watch(
             <th class="t-header">Branch</th>
             <th class="t-header">Email</th>
             <th class="t-header">Phone</th>
-            <th class="text-center">Role</th>
+            <th class="text-center">Services Offered</th>
+            <th class="text-center">Current Float</th>
             <th class="text-center">Status</th>
             <th class="text-center">Activation</th>
             <th class="text-center">Date</th>

733ffe827132af31af99397d3df40d0446e8b6e5
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-28 16:17:20 +0300

    makes dashboard tiles

--- src/domain/services/components/CreateService.vue
+++ src/domain/services/components/CreateService.vue
@@ -83,7 +83,7 @@ watch(
 <template>
   <div class="bg-white py-5">
     <p class="text-xl font-bold"> Create a Service</p>
-    <p class="text-sm text-gray-500">A designated location managed by a Super Agent to provide services to agents and the public.</p>
+    <p class="text-sm text-gray-500">A public good consumed and/or paid for.</p>
     <form @submit.prevent="submit" class="pt-5">
       <div class="flex">
         <div class="cell-full">

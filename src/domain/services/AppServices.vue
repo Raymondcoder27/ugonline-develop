f27663ce2ccb975ef2248eb09c139928280e8ab6
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-29 17:34:02 +0300

    restyles User interface

--- src/domain/services/AppServices.vue
+++ src/domain/services/AppServices.vue
@@ -219,30 +219,32 @@ watch(
       </div>
     </div>
   </div>
-  <div class="w-4/12">
+ <div class="block w-4/12">
+  <div class="">
       <div class="w-12/12 count">
         <p class="text-xl font-bold py-2">13</p>
         <p class="text-xs">Company Incorporation</p>
       </div>
     </div>
-    <div class="w-4/12">
+    <div class="">
       <div class="w-12/12 count">
         <p class="text-xl font-bold py-2">13</p>
         <p class="text-xs">Name Reservation</p>
       </div>
     </div>
-    <div class="w-4/12">
+    <div class="">
       <div class="w-12/12 count">
         <p class="text-xl font-bold py-2">13</p>
         <p class="text-xs">Company Insolvency</p>
       </div>
     </div>
-    <div class="w-4/12">
+    <div class="">
       <div class="w-12/12 count">
         <p class="text-xl font-bold py-2">13</p>
         <p class="text-xs">File Resolution</p>
       </div>
     </div>
+ </div>
  </div>
 
   <!-- Modal -->

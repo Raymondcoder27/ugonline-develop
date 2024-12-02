8d65548ef1d3b4612ce74cc5e80ff24371ebaebf
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-02 15:04:18 +0300

    reworks billing

--- src/domain/agents/BackofficeAccounts.vue
+++ src/domain/agents/BackofficeAccounts.vue
@@ -116,10 +116,10 @@ watch(
     <div class="flex space-x-2 my-1 pt-1 pb-3">
       <div class="flex-grow">
         <div class="grid grid-cols-5 gap-2 bg-gray-10 border border-gray-200 rounded px-2 py-3">
-          <input v-if="filter.filter !== undefined" input-type="text" v-model="filter.filter[0].operand"
+          <!-- <input v-if="filter.filter !== undefined" input-type="text" v-model="filter.filter[0].operand"
             class="filter-element e-input" type="text" placeholder="Search by Name" />
           <input v-if="filter.filter !== undefined" input-type="text" v-model="filter.filter[1].operand"
-            class="filter-element e-input" type="text" placeholder="Branch" />
+            class="filter-element e-input" type="text" placeholder="Branch" /> -->
           <input v-if="filter.filter !== undefined" input-type="text" v-model="filter.filter[2].operand"
             class="filter-element e-input" type="text" placeholder="Till Number" />
           <!-- <select class="filter-element e-select">

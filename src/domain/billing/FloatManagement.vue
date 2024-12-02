df6d2828c040e70ab3a947b78c79746b5cf8cddc
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-02 11:07:22 +0300

    reworks billing

--- src/domain/billing/FloatManagement.vue
+++ src/domain/billing/FloatManagement.vue
@@ -140,12 +140,12 @@ watch(
           <tr class="header-tr">
 <!--            <th class="t-header">#</th>-->
             <th class="t-header" width="30%">Names</th>
-            <th class="t-header">Email</th>
-            <th class="t-header">Phone</th>
-            <th class="text-center">Role</th>
-            <th class="text-center">Status</th>
-            <th class="text-center">Activation</th>
-            <th class="text-center">Date</th>
+            <th class="t-header">Branch</th>
+            <!-- <th class="t-header">Phone</th> -->
+            <!-- <th class="text-center">Role</th> -->
+            <!-- <th class="text-center">Status</th> -->
+            <!-- <th class="text-center">Activation</th> -->
+            <th class="text-center">Date Assigned</th>
             <th class="t-header"></th>
           </tr>
         </thead>

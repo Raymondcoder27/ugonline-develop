db2dedf2b06c5523b8f319b72fb2a32b93673d5d
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-02 18:29:29 +0300

    reworks billing

--- src/domain/services/AppServices.vue
+++ src/domain/services/AppServices.vue
@@ -13,6 +13,8 @@ import CategorySelector from "@/domain/settings/components/CategorySelector.vue"
 import { useNotificationsStore } from "@/stores/notifications";
 import type { ApiError } from "@/types";
 import TableLoader from "@/components/TableLoader.vue";
+import { defineComponent } from 'vue';
+
 
 const store = useServicesStore();
 const modalOpen: Ref<boolean> = ref(false);
@@ -109,6 +111,106 @@ watch(
     }
   }
 );
+
+
+export default defineComponent({
+  name: 'Dashboard',
+  data() {
+    return {
+      services: [
+        {
+          id: 1,
+          service: 'Post Office Account',
+          name: 'Posta Uganda',
+          thumbnail: '/assets/coa-19c4edfc.png',
+          email: 'info@ugapost.co.ug',
+          phone: '+256784944479',
+          address: 'Plot 35 Kampala Road, Kampala',
+        },
+        {
+          id: 2,
+          service: 'Agricultural Research',
+          name: 'National Agricultural Research Organization',
+          thumbnail: '/assets/coa-19c4edfc.png',
+          email: 'info@naro.go.ug',
+          phone: '+256-41-320512',
+          address: 'Plot 11-13, Lugard Avenue, Entebbe',
+        },
+        {
+          id: 3,
+          service: 'National ID Registration',
+          name: 'National Identification & Registration Authority',
+          // thumbnail: 'https://nira.go.ug/assets/exports/nira_logo_black@2x.png',
+          thumbnail: '/assets/coa-19c4edfc.png',
+          email: 'info@nira.com',
+          phone: '0800211700',
+          address: 'National Independence Grounds, Kololo Airstrip',
+        },
+        {
+          id: 4,
+          service: 'Land Title Registration',
+          name: 'Ministry of Lands and Urban Development',
+          thumbnail: '/assets/coa-19c4edfc.png',
+          email: 'info@mlhud.go.ug',
+          phone: '0414355355',
+          address: 'Dewinton Road',
+        },
+        {
+          id: 5,
+          service: 'Name Reservation',
+          name: 'URSB',
+          // thumbnail: 'https://obrs.ursb.go.ug/img/full_logo.9fe8e881.jpg',
+          thumbnail: '/assets/coa-19c4edfc.png',
+          email: 'ursb@ursb.go.ug',
+          phone: '0312211211',
+          address: 'Plot 1 Baskerville Avenue',
+        },
+        // {
+        //   id: 6,
+        //   service: 'Name Reservation',
+        //   name: 'Ministry of Foreign Affairs',
+        //   thumbnail: '/assets/coa-19c4edfc.png',
+        //   email: 'info@mofa.go.ug',
+        //   phone: '0312411403',
+        //   address: 'Parliamentary Avenue',
+        // },
+        // {
+        //   id: 7,
+        //   name: 'Ministry of Agriculture, Animal Industry and Fisheries',
+        //   thumbnail: '/assets/coa-19c4edfc.png',
+        //   email: 'info@maaif.go.ug',
+        //   phone: '0312123123',
+        //   address: 'Kabanyoro',
+        // },
+        // {
+        //   id: 8,
+        //   name: 'National Water and Sewage Corporation',
+        //   thumbnail: '/assets/coa-19c4edfc.png',
+        //   email: 'info@nwsc.go.ug',
+        //   phone: '0312123123',
+        //   address: '',
+        // },
+        {
+          id: 9,
+          service: 'Passport Application',
+          name: 'Ministry of Internal Affairs',
+          thumbnail: '/assets/coa-19c4edfc.png',
+          email: 'info@moia.go.ug',
+          phone: '0312123123',
+          address: '',
+        },
+        // {
+        //   id: 10,
+        //   name: 'Ministry of Gender, Labour and Social Development',
+        //   thumbnail: '/assets/coa-19c4edfc.png',
+        //   email: 'info@moglsd.go.ug',
+        //   phone: '0312123123',
+        //   address: '',
+        // },
+      ],
+    };
+  },
+});
 </script>
 
 <template>

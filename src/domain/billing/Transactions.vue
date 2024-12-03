51e55eac0bd0e1d2e759a4473cca69be486bd6af
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-03 12:45:18 +0300

    reworks billing

--- src/domain/billing/Transactions.vue
+++ src/domain/billing/Transactions.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted, watch } from "vue";
 import AppModal from "@/components/AppModal.vue";
-import { useBilling } from "@/domain/billing/stores"; 
+import { useBilling } from "@/domain/billing/stores";
 import { useDebounceFn } from "@vueuse/core";
 import moment from "moment";
 import type { IGoFilter } from "@/types";
@@ -21,14 +21,14 @@ const filter: IGoFilter = reactive({
   sort: [
     {
       field: "date", // Sorting by date for transactions
-      order: "ASC"
-    }
+      order: "ASC",
+    },
   ],
   filter: [
     { field: "branchName", operand: "", operator: "CONTAINS" },
     { field: "manager", operand: "", operator: "CONTAINS" },
-    { field: "status", operand: "", operator: "CONTAINS" }
-  ]
+    { field: "status", operand: "", operator: "CONTAINS" },
+  ],
 });
 
 // Fetch transactions
@@ -66,9 +66,12 @@ const close = () => {
 };
 
 // Watchers
-watch(() => modalOpen.value, (isOpen) => {
-  if (!isOpen) fetch(); // Re-fetch transactions when modal closes
-});
+watch(
+  () => modalOpen.value,
+  (isOpen) => {
+    if (!isOpen) fetch(); // Re-fetch transactions when modal closes
+  }
+);
 
 watch(() => filter, updateFilter, { deep: true });
 
@@ -80,7 +83,8 @@ onMounted(() => fetch());
 <template>
   <div class="w-full shadow-lg bg-white rounded p-2 h-full">
     <div class="flex space-x-2 my-1 pt-1 pb-3">
-      <div class="flex-grow"></div><!-- <div class="flex-grow">
+      <div class="flex-grow"></div>
+      <!-- <div class="flex-grow">
         <div class="grid grid-cols-5 gap-2 bg-gray-10 border border-gray-200 rounded px-2 py-3">
           <input v-if="filter.filter !== undefined" input-type="text" v-model="filter.filter[0].operand"
             class="filter-element e-input" type="text" placeholder="Search by Name" />
@@ -108,7 +112,7 @@ onMounted(() => fetch());
         </thead>
         <tbody>
           <tr
-          class="text-left"
+            class="text-left"
             v-for="(transaction, idx) in billingStore.transactions"
             :key="idx"
             :class="transaction.status === 'BLOCKED' ? 'blocked' : ''"
@@ -119,36 +123,35 @@ onMounted(() => fetch());
             <td class="text-left">{{ transaction.status }}</td>
             <td class="text-left">{{ convertDate(transaction.date) }}</td>
             <!-- <td class="text-left"> -->
-              <!-- <button @click="openTransaction(transaction)">Edit</button> -->
-               <!-- actions -->
+            <!-- <button @click="openTransaction(transaction)">Edit</button> -->
+            <!-- actions -->
 
             <!-- </td> -->
 
             <td class="text-left">
-  <i
-    class="fa-solid fa-eye p-1 mx-1 text-blue-600 bg-blue-100 border border-blue-200 hover:text-blue-700"
-    @click="open(transaction)"
-  ></i>
-  <i
-    class="fa-solid fa-pen p-1 mx-1 text-green-600 bg-green-100 border border-green-200 hover:text-green-700"
-    @click="edit(transaction)"
-  ></i>
-  <!-- <i
+              <i
+                class="fa-solid fa-eye p-1 mx-1 text-blue-600 bg-blue-100 border border-blue-200 hover:text-blue-700"
+                @click="open(transaction)"
+              ></i>
+              <i
+                class="fa-solid fa-pen p-1 mx-1 text-green-600 bg-green-100 border border-green-200 hover:text-green-700"
+                @click="edit(transaction)"
+              ></i>
+              <!-- <i
     class="fa-solid fa-sliders p-1 mx-1 text-primary-700 bg-primary-100 border border-primary-300 hover:text-primary-900"
     @click="configure(branch)"
   ></i> -->
 
-  <!-- delete branch -->
-   <i
-    class="fa-solid fa-trash p-1 mx-1 text-red-600 bg-red-100 border border-red-200 hover:text-red-700"
-    @click="deleteBranch(transaction)"
-  ></i>
-</td>
+              <!-- delete branch -->
+              <i
+                class="fa-solid fa-trash p-1 mx-1 text-red-600 bg-red-100 border border-red-200 hover:text-red-700"
+                @click="deleteBranch(transaction)"
+              ></i>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
-
 
     <!-- <div class="flex">
       <button v-if="page > 1" @click="previousPage">Previous</button>

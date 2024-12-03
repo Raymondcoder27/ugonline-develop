49f292cedf31b16663a5fbe9a1c538f6557b7cd0
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-03 10:27:25 +0300

    reworks billing

--- src/domain/branches/AppBranches.vue
+++ src/domain/branches/AppBranches.vue
@@ -314,9 +314,15 @@ watch(
     class="fa-solid fa-pen p-1 mx-1 text-green-600 bg-green-100 border border-green-200 hover:text-green-700"
     @click="edit(branch)"
   ></i>
-  <i
+  <!-- <i
     class="fa-solid fa-sliders p-1 mx-1 text-primary-700 bg-primary-100 border border-primary-300 hover:text-primary-900"
     @click="configure(branch)"
+  ></i> -->
+
+  <!-- delete branch -->
+   <i
+    class="fa-solid fa-trash p-1 mx-1 text-red-600 bg-red-100 border border-red-200 hover:text-red-700"
+    @click="deleteBranch(branch)"
   ></i>
 </td>
 

9fd2f5f750d9dbbb4e718f3dc2f045977158384e
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-03 20:15:18 +0300

    reworks billing

--- src/domain/billing/FloatManagement.vue
+++ src/domain/billing/FloatManagement.vue
@@ -151,7 +151,7 @@ watch(
             <th class="t-header">Branch</th>
             <th class="t-header">Amount(UGX)</th>
             <th class="text-left">Date Assigned</th>
-            <th class="t-header">Actions</th>
+            <!-- <th class="t-header">Actions</th> -->
             <th class="t-header"></th>
 
           </tr>
@@ -169,7 +169,7 @@ watch(
             <td class="text-left">{{ convertDateTime(assignment.createdAt) }}</td>
 
 
-            <td class="text-left">
+            <!-- <td class="text-left">
               <i
                 class="fa-solid fa-eye p-1 mx-1 text-blue-600 bg-blue-100 border border-blue-200 hover:text-blue-700"
                 @click="open(assignment)"
@@ -178,17 +178,11 @@ watch(
                 class="fa-solid fa-pen p-1 mx-1 text-green-600 bg-green-100 border border-green-200 hover:text-green-700"
                 @click="edit(assignment)"
               ></i>
-              <!-- <i
-    class="fa-solid fa-sliders p-1 mx-1 text-primary-700 bg-primary-100 border border-primary-300 hover:text-primary-900"
-    @click="configure(branch)"
-  ></i> -->
-
-              <!-- delete branch -->
               <i
                 class="fa-solid fa-trash p-1 mx-1 text-red-600 bg-red-100 border border-red-200 hover:text-red-700"
                 @click="deleteRequest(transaction)"
               ></i>
-            </td>
+            </td> -->
           </tr>
         </tbody>
       </table>

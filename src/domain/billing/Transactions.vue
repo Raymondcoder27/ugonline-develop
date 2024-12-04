5037bbb2e30309d1d1317a27360660e0d0bb5b55
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-04 16:09:15 +0300

    fixes billing

--- src/domain/billing/Transactions.vue
+++ src/domain/billing/Transactions.vue
@@ -155,7 +155,7 @@ onMounted(() => {
             :class="transaction.status === 'BLOCKED' ? 'blocked' : ''"
           >
             <!-- <td>{{ idx + 1 }}</td> -->
-            <td class="bg-red-200 rounded-md font-semibold text-red-700 p-1 hover:underline">{{ transaction.trackingNumber }}</td>
+            <td class="rounded-md font-semibold text-red-700 hover:underline">{{ transaction.trackingNumber }}</td>
             <td>{{ transaction.service }}</td>
             <td class="text-left">{{ transaction.provider }}</td>
             <td>{{ transaction.till }}</td>

1f5fec8cf9a50b979d77799089a84b061582e2fb
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-04 12:59:50 +0300

    fixes billing

--- src/domain/accounts/components/CreateAccount.vue
+++ src/domain/accounts/components/CreateAccount.vue
@@ -33,7 +33,7 @@ function submit() {
 
 <template>
   <div class="bg-white py-5">
-    <p class="text-xl font-bold"> Create an Account</p>
+    <p class="text-xl font-bold"> Create a BackOffice Account</p>
     <p class="text-sm text-gray-500">Administrators who will manage this portal and handle providers and services</p>
     <form @submit.prevent="submit" class="pt-5">
       <div class="flex">
@@ -50,12 +50,12 @@ function submit() {
             required />
         </div>
       </div>
-      <div class="flex">
+      <!-- <div class="flex">
         <div class="cell-full">
           <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Middle Name</label>
           <input autocomplete="off" type="text" v-model="form.middleName" class="noFocus form-element e-input w-full" />
         </div>
-      </div>
+      </div> -->
 
       <div class="flex">
         <div class="cell">

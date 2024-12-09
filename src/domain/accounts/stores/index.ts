c1a2a6e130397dc8f5c0fe26a2d4ea4bd32b0e99
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-09 17:23:24 +0300

    reworks billing

--- src/domain/accounts/stores/index.ts
+++ src/domain/accounts/stores/index.ts
@@ -62,12 +62,12 @@ export const useAccounts = defineStore("user-management", () => {
 
   const dummyBackofficeAccounts: Account[] = [
     {
-      firstName: "Admin", lastName: "User", middleNames: "B", username: "admin.user@example.com",
+      firstName: "Jack", lastName: "Tumwine", middleNames: "B", username: "jacktumwine.user@example.com",
       phone: "345-678-9012", role: "admin", createdAt: "2021-03-01",
       emailVerified: true, phoneVerified: true, activatedAt: "2021-03-01", email: "admin.user@example.com"
     },
     {
-      firstName: "Support", lastName: "Agent", middleNames: "C", username: "support.agent@example.com",
+      firstName: "Sarah", lastName: "Nambasa", middleNames: "C", username: "sarahnambasa.agent@example.com",
       phone: "456-789-0123", role: "admin", createdAt: "2021-04-01",
       emailVerified: true, phoneVerified: true, activatedAt: "2021-04-01", email: "support.agent@example.com"
     }

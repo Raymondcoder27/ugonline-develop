27961103fd126bf99eba652b449219009d6f7a55
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-09 17:04:34 +0300

    reworks billing

--- src/domain/billing/components/AllocateFloat.vue
+++ src/domain/billing/components/AllocateFloat.vue
@@ -124,11 +124,24 @@ onMounted(() => {
 
       <div class="flex">
         <div class="cell-full">
-          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1">Amount (UGX)</label>
-          <input autocomplete="off" type="number" v-model="form.amount" class="noFocus form-element e-input w-full"
-            required />
+          <label class="block uppercase text-neutral-600 text-xs font-bold mb-1"
+            >Amount (UGX)</label
+          >
+          <input
+            autocomplete="off"
+            type="number"
+            v-model.number="form.amount"
+            class="noFocus form-element e-input w-full"
+            required
+          />
+          <span v-if="form.amount">
+            <pre class="font-semibold text-green-600 py-2"
+              >{{ form.amount.toLocaleString() }}/=</pre
+            >
+          </span>
         </div>
       </div>
+
       
       <div class="flex">
         <!-- <div class="cell">

58507be044a360a4758f1997e4aba6bf246fa8b6
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-09 14:49:43 +0300

    reworks billing

--- src/domain/billing/stores/index.ts
+++ src/domain/billing/stores/index.ts
@@ -25,15 +25,15 @@ export const useBilling = defineStore("billing", () => {
   const dummyTransactions: Transaction[] = [
     { id: 1, trackingNumber: "TA123456",
       service: "Company Name Reservation", provider: "URSB", till: "Till 001",
-      fee: 25000, date: "2021-09-01", status: "Success" 
+      fee: 25000, date: "2021-09-01", status: "success" 
     },
     { id: 2, trackingNumber: "TB123457",
       service: "Create Postal Account", provider: "Posta Uganda",
-       till: "Till 002", fee: 20000, date: "2021-09-02", status: "Success"
+       till: "Till 002", fee: 20000, date: "2021-09-02", status: "failed"
       },
     { id: 3, trackingNumber: "TC123458",
       service: "National ID registration", provider: "NIRA",
-       till: "Till 003", fee: 35000, date: "2021-09-03", status: "Success" },
+       till: "Till 003", fee: 35000, date: "2021-09-03", status: "pending" },
   ];
 
 

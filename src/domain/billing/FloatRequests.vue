a9d452d642849c063a5b9868d61da9d5043f6e4f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-10 16:05:42 +0300

    reworks billing

--- src/domain/billing/FloatRequests.vue
+++ src/domain/billing/FloatRequests.vue
@@ -175,7 +175,7 @@ onMounted(() => {
                 <!-- <td> -->
                 <!-- <label> -->
                 <span
-                  class="text-xs cursor-pointer rounded-md p-1 font-semibold text-green-600 bg-green-100 border border-green-200 hover:text-green-700 hover:bg-green-200"
+                  class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-green-600 bg-green-100 border border-green-200 hover:text-green-700 hover:bg-green-200"
                   @click="open(request)"
                   >Approved</span
                 >
@@ -188,7 +188,7 @@ onMounted(() => {
                 <!-- <td> -->
                 <label>
                   <span
-                    class="text-xs cursor-pointer rounded-md p-1 font-semibold text-red-600 bg-red-100 border border-red-200 hover:text-red-700 hover:bg-red-200"
+                    class="text-xs cursor-pointer rounded-md px-1 py-0.5 font-semibold text-red-600 bg-red-100 border border-red-200 hover:text-red-700 hover:bg-red-200"
                     @click="open(request)"
                     >Rejected</span
                   >
@@ -200,13 +200,13 @@ onMounted(() => {
               <div v-else>
                 <!-- <td> -->
                 <span
-                  class="text-xs rounded-md p-1 font-semibold text-blue-600 bg-blue-100 border border-blue-200 hover:text-blue-700 hover:bg-blue-200"
+                  class="text-xs rounded-md px-1 py-0.5 font-semibold text-blue-600 bg-blue-100 border border-blue-200 hover:text-blue-700 hover:bg-blue-200"
                   @click="approveFloatRequest(request.id)"
                   >Approve</span
                 >
 
                 <span
-                  class="text-xs rounded-md p-1 ml-3 font-semibold text-red-600 bg-red-100 border border-red-200 hover:text-red-700 hover:bg-red-200"
+                  class="text-xs rounded-md px-1 py-0.5 ml-3 font-semibold text-red-600 bg-red-100 border border-red-200 hover:text-red-700 hover:bg-red-200"
                   @click="rejectFloatRequest(request.id)"
                   >Reject</span
                 >

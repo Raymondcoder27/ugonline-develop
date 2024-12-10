050b9eed1d4e209890966c366345c1245b64c58d
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-10 11:34:09 +0300

    fixes billing

--- src/domain/services/AppServices.vue
+++ src/domain/services/AppServices.vue
@@ -15,6 +15,8 @@ import type { ApiError } from "@/types";
 import TableLoader from "@/components/TableLoader.vue";
 import { useDebounceFn } from "@vueuse/core";
 // import { subscribe } from "diagnostics_channel";
+import ServiceCards from "@/domain/services/components/ServiceCards.vue";
+import SubscribedServices from "@/domain/services/components/SubscribedServices.vue";
 
 const store = useServicesStore();
 const modalOpen: Ref<boolean> = ref(false);
@@ -292,48 +294,10 @@ function unsubscribeFromService(serviceId: string) {
       </div>
 
       <!-- Service Cards Section -->
-      <div class="grid grid-cols-4 gap-3">
-  <!-- Service Tile -->
-  <div
-    v-for="service in store.services"
-    :key="service.id"
-    class="service service-active p-4 bg-white shadow rounded transform transition duration-300 ease-in-out hover:scale-105 flex flex-col justify-between"
-  >
-    <!-- Top Section -->
-    <div>
-      <div class="flex justify-between items-center">
-        <img
-          :src="service.thumbnail"
-          alt=""
-          class="w-10 h-10 object-cover"
-        />
-      </div>
-      <hr class="my-2" />
-      <div class="text-bold text-sm font-bold" v-if="service">
-        {{ service.name }}
-      </div>
-      <div v-else>
-        <p>Service not found.</p>
-      </div>
-      <div class="h-15 overflow-hidden">
-        <span class="text-xs text-gray-800">
-          {{ service.description }}
-        </span>
-      </div>
+      <div class="block w-2/3">
+      <!-- Service Cards Area -->
+      <ServiceCards :services="store.services" @subscribe="subscribe" />
     </div>
-
-    <!-- Bottom Section -->
-    <div class="mt-4">
-      <p
-        class="font-bold text-xs w-full text-white bg-red-700 p-1 rounded-md text-center cursor-pointer hover:scale-105 hover:bg-red-800"
-        @click="subscribe(service.id)"
-      >
-        <i class="fa fa-check-square"></i>
-        Subscribe
-      </p>
-    </div>
-  </div>
-</div>
 
     </div>
 

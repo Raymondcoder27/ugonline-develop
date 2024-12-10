bb3565c4786730c4a2ef1dea42dd895c52402647
RaymondCoder27 <mwebraymond13@gmail.com>
2024-12-10 10:01:57 +0300

    reworks billing

--- src/domain/services/AppServices.vue
+++ src/domain/services/AppServices.vue
@@ -420,11 +420,7 @@ function unsubscribeFromService(serviceId: string) {
     </button> -->
         </div>
       </div>
-
-      <!-- Service Cards Section -->
-      <div class="grid grid-cols-4 gap-3">
-        <!-- v-if service has been subscribed, its removed from the array -->
-        <div class="flex justify-center items-center mt-4">
+      <div class="flex justify-center items-center mt-4">
   <!-- Previous Button -->
   <button
     class="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 disabled:opacity-50 disabled:cursor-not-allowed"
@@ -449,6 +445,11 @@ function unsubscribeFromService(serviceId: string) {
   </button>
 </div>
 
+
+      <!-- Service Cards Section -->
+      <div class="grid grid-cols-4 gap-3">
+        <!-- v-if service has been subscribed, its removed from the array -->
+         
         <div
           v-for="service in store.services"
           :key="service.id"
